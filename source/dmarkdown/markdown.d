ae656e53be7ad272196707df23804462ad6d4be5
Ferdinand Majerech <kiithsacmp@gmail.com>
2015-02-21 14:51:47 +0100

    Documentation update

--- source/dmarkdown/markdown.d
+++ source/dmarkdown/markdown.d
@@ -476,7 +476,7 @@ pure @safe {
 	}
 }
 
-/// private
+// private
 private void writeBlock(R)(ref R dst, ref const Block block, LinkRef[string] links, scope MarkdownSettings settings)
 {
 	final switch(block.type){
@@ -565,7 +565,7 @@ private void writeMarkdownEscaped(R)(ref R dst, ref const Block block, in LinkRe
 	if (lines.length) dst.put("\n");
 }
 
-/// private
+// private
 private void writeMarkdownEscaped(R)(ref R dst, string ln, in LinkRef[string] linkrefs, scope MarkdownSettings settings)
 {
 	bool br = ln.endsWith("  ");
